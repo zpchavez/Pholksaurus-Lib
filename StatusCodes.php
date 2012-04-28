099bb23bef1e4bb31b06649053e548867615e762
Zachary Chavez <zach@rychavez.com>
2012-04-27 20:35:05 -0700

    Added status code constants for 400 and 500.

--- StatusCodes.php
+++ StatusCodes.php
@@ -9,9 +9,11 @@ class StatusCodes
     const OK                 = 200;
     const CREATED            = 201;
     const NOT_MODIFIED       = 304;
+    const BAD_REQUEST        = 400;
     const FORBIDDEN          = 403;
     const NOT_FOUND          = 404;
     const METHOD_NOT_ALLOWED = 405;
     const CONFLICT           = 409;
     const GONE               = 410;
+    const SERVER_ERROR       = 500;
 }
